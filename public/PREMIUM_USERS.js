fef29db6aabea24ee905cc9d7fe925fa641fb159
arulzxd <arulzz.xd@gmail.com>
2026-07-30 23:55:35 +0700

    Add files via upload

---
+++ public/PREMIUM_USERS.js
@@ -0,0 +1,6 @@
+const PREMIUM_USERS = [
+    "username_premium", // masukin username yang mau menjadi user premium
+    "username_premium" // masukin username yang mau menjadi user premium
+];
+
+module.exports = PREMIUM_USERS;
---
+++ public/VIP_USERS.js
@@ -0,0 +1,6 @@
+const VIP_USERS = {
+    "username_vip": "your_custom_apikey", // masukin username yang mau menjadi user atau email vip
+    "email_user_vip@gmail.com": "your_custom_apikey" // masukin username yang mau menjadi user atau email vip
+};
+
+module.exports = VIP_USERS;
