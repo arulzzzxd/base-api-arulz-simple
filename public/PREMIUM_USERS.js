c84645ac478ea9cb73dda9dee3588eccd06c8cdd
arulzxd <arulzz.xd@gmail.com>
2026-07-30 23:57:02 +0700

    Delete public directory

--- public/PREMIUM_USERS.js
+++
@@ -1,6 +0,0 @@
-const PREMIUM_USERS = [
-    "username_premium", // masukin username yang mau menjadi user premium
-    "username_premium" // masukin username yang mau menjadi user premium
-];
-
-module.exports = PREMIUM_USERS;
--- public/VIP_USERS.js
+++
@@ -1,6 +0,0 @@
-const VIP_USERS = {
-    "username_vip": "your_custom_apikey", // masukin username yang mau menjadi user atau email vip
-    "email_user_vip@gmail.com": "your_custom_apikey" // masukin username yang mau menjadi user atau email vip
-};
-
-module.exports = VIP_USERS;
